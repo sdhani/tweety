7cc13e2810fdc9df2586c8b33783f33347bc4775
sdhani <shania.dhani46@myhunter.cuny.edu>
2020-10-03 22:06:35 -0400

    added twitter api for name, and tweet, need to fix image

---
+++ Twitter/TweetsCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetsCellTableViewCell.swift
+//  Twitter
+//
+//  Created by Shania Dhani on 10/3/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetsCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
